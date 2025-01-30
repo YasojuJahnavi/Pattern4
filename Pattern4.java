a9ff7f6d9f2bb5ef79f48cbf228f0fe9b3c94edd
YasojuJahnavi <yasojujahnavi.jh7@gmail.com>
2025-01-30 19:29:12 +0530

    Add files via upload

---
+++ Pattern4.java
@@ -0,0 +1,17 @@
+public class Pattern4 {
+    public static void main(String[] args){
+    int n=4;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){
+            if(i==1&&(j==3||j==4)){
+                System.out.print("*");
+            }else if (i==3&&j==4){
+                System.out.print("*");
+            }else{
+                System.out.print("#");
+            }
+        }
+        System.out.println();
+}
+}
+}
